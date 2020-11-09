c3b52d1ec1bf6c2a522c4476bfe7cd14d47e07fe
AndersenCastaneda <34457834+AndersenCastaneda@users.noreply.github.com>
2020-11-09 05:34:21 -0500

    Add more test cases

--- Assets/Scripts/GameSystems/HealthSystem.cs
+++ Assets/Scripts/GameSystems/HealthSystem.cs
@@ -1,39 +1,28 @@
-﻿using System;
+﻿#pragma warning disable 0649
+
 using UnityEngine;
 
 namespace TestUnity.GameSystems
 {
     public sealed class HealthSystem : MonoBehaviour, IHealth
     {
-//#pragma warning disable 0649
-//        [SerializeField] private IntRefference health;
-//#pragma warning restore 0649
-
+#if TESTING
         public IntRefference health;
+        public IntRefference maxHealth;
+#else
+        [SerializeField] private IntRefference health;
+        [SerializeField] private readonly IntRefference maxHealth;
+#endif
 
         public void TakeDamage(int damage)
         {
-            health.value = (damage > health.value) ? 0 : health.value - damage;
+            health.value = (damage >= health.value) ? 0 : health.value - damage;
         }
 
         public void TakeCure(int cure)
         {
-            health.value = (health.value + cure > 100) ? 100 : health.value + cure;
-        }
-    }
-
-    public struct HealthSystemStruct : IHealth
-    {
-        public IntRefference health;
-
-        public void TakeDamage(int damage)
-        {
-            health.value = (damage > health.value) ? 0 : health.value - damage;
-        }
-
-        public void TakeCure(int cure)
-        {
-            health.value = (health.value + cure > 100) ? 100 : health.value + cure;
+            if (health.value > 0)
+                health.value = (health.value + cure >= maxHealth.value) ? maxHealth.value : health.value + cure;
         }
     }
 }
---
+++ Assets/Scripts/UISystems/UIHealth.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable 0649
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestUnity.UISystems
+{
+    public sealed class UIHealth : MonoBehaviour
+    {
+#if TESTING
+        public Slider slider;
+        public IntRefference health;
+        public IntRefference maxHealth;
+#else
+        [SerializeField] private Slider slider;
+        [SerializeField] private IntRefference health;
+        [SerializeField] private IntRefference maxHealth;
+#endif
+
+        public void InitComponent()
+        {
+            if (slider == null)
+                slider = GetComponent<Slider>();
+
+            slider.minValue = 0;
+            slider.maxValue = maxHealth.value;
+            slider.value = health.value;
+        }
+
+        public void UpdateInfo() => slider.value = health.value;
+
+        private void Awake() => InitComponent();
+
+        private void Update() => UpdateInfo();
+    }
+}
